c954b5975362fa0f3b2c992a176d9ef7a2fdacaa
LucasDoyen <lucass.m.d@hotmail.com>
2022-09-11 23:42:50 -0300

    js

---
+++ variables.js
@@ -0,0 +1,11 @@
+
+var pais = "argentina";
+var continente= "suramerica";
+var antiguedad= "2022";
+var pais_y_contionente = (pais + antiguedad + continente);
+
+pais = "francia";
+continente = "europa";
+
+console.log(pais, continente, antiguedad);
+ alert("pais_y_contionente");
